671ac5e7579209aada780857a46837b6ab9be0e2
Ulissestsaraujo <ulissestsaraujo@github.com>
2023-06-24 22:31:09 +0200

    sec 3

---
+++ client/src/features/catalog/ProductCard.tsx
@@ -0,0 +1,47 @@
+import * as React from "react";
+import Card from "@mui/material/Card";
+import CardActions from "@mui/material/CardActions";
+import CardContent from "@mui/material/CardContent";
+import CardMedia from "@mui/material/CardMedia";
+import Button from "@mui/material/Button";
+import Typography from "@mui/material/Typography";
+import { Product } from "../../app/models/product";
+import { Avatar, CardHeader } from "@mui/material";
+
+type IProductProps = {
+  product: Product;
+};
+export default function ProductCard({ product }: IProductProps) {
+  return (
+    <Card>
+      <CardHeader
+        avatar={<Avatar>{product.name.charAt(0).toUpperCase()}</Avatar>}
+        title={product.name}
+        titleTypographyProps={{
+          sx: { fontWeight: "bold", bgcolor: "secondary.main" },
+        }}
+      />
+      <CardMedia
+        sx={{
+          height: 140,
+          backgroundSize: "contain",
+          bgcolor: "primary.light",
+        }}
+        image={product.pictureUrl}
+        title={product.name}
+      />
+      <CardContent>
+        <Typography gutterBottom color="secondary" variant="h5" component="div">
+          ${(product.price / 100).toFixed(2)}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          {product.brand} / {product.type}
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Add to cart</Button>
+        <Button size="small">View</Button>
+      </CardActions>
+    </Card>
+  );
+}
