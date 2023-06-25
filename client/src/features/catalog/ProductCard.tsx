c49efe982d2716a588ba0cb5af15fc94924c48ff
Ulissestsaraujo <ulissestsaraujo@github.com>
2023-06-25 13:14:29 +0200

    Finished routing

--- client/src/features/catalog/ProductCard.tsx
+++ client/src/features/catalog/ProductCard.tsx
@@ -7,6 +7,7 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import { Product } from "../../app/models/product";
 import { Avatar, CardHeader } from "@mui/material";
+import { Link } from "react-router-dom";
 
 type IProductProps = {
   product: Product;
@@ -40,7 +41,9 @@ export default function ProductCard({ product }: IProductProps) {
       </CardContent>
       <CardActions>
         <Button size="small">Add to cart</Button>
-        <Button size="small">View</Button>
+        <Button component={Link} to={`/catalog/${product.id}`} size="small">
+          View
+        </Button>
       </CardActions>
     </Card>
   );
